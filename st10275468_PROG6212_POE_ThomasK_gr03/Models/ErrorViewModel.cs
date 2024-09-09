52a0b8640b6d5a3a074d4b7a3c8e76f80f10b219
st10275468 <st10275468@vcconnect.edu.za>
2024-09-09 20:23:40 +0200

    Add project files.

---
+++ st10275468_PROG6212_POE_ThomasK_gr03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace st10275468_PROG6212_POE_ThomasK_gr03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
